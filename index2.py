6d9525d780159471e7ab9404938fdfe2574f133c
LiuDehuaIsMe <2388540124@qq.com>
2022-08-30 10:25:44 +0800

    更改

--- index2.py
+++ index2.py
@@ -5,14 +5,26 @@ import time
 
 import execjs
 
-content = open('./wocalie2.js','r',encoding='utf-8').read()
-regex = re.compile(r"[\xA0\u1680\u180E\u2000-\u200A\u202f\u205f]")
+node = execjs.get() 
+
+wodiu2 = r"""
+
+"""
+# regex = re.compile(r"[\xA0\u1680\u180E\u2000-\u200A\u202f\u205f]")
+regex = re.compile(r"[\xA0\u1680\u2000-\u200A\u202f\u205f]")
 ctx = execjs.compile(open('./create_page_session_id.js','r',encoding='utf-8').read())
-ctx2 = execjs.compile(regex.sub("",content))
+# ctx2 = execjs.compile(regex.sub("",content))
+# wodiu = open('./wocalie2.js','r',encoding='utf-8').read() .encode('GBK','ignore').decode('GBk') 
+wodiu = open('./wocalie4.js','r',encoding='utf-8').read() 
+# print(wodiu)
+ctx2 = execjs.compile(wodiu)
+# ctx3 = execjs.compile(open('./wocalie3.js','a+',encoding='utf-8').read())
+
 sign = ctx.eval("le()")
 
 encParams = ctx2.eval("createEncParams()")
-# print(sign)
+# encParams = ctx2.eval("createEncParams()")
+print(sign)
 # print(encParams)
 class SimulateLogin:
 
--- wocalie3.js
+++ wocalie3.js
@@ -1,3 +1,4 @@
-function wodiu() {
-	return 12
+// console.log(window.URSSM4.encrypt(str, "BC60B8B9E4FFEFFA219E5AD77F11F9E2"));
+function createEncParams() {
+	return 222;
 }
